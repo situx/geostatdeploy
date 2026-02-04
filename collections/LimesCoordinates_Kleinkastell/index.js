0da50420c5c2f4ca45d244eb82b81ca30d20759f
situx <situx@users.noreply.github.com>
2026-02-04 15:38:31 +0000

    Deploying to gh-pages from  @ 33bb5adb06680bdd210f71b05b0ce483204d00d1 🚀

---
+++ collections/LimesCoordinates_Kleinkastell/index.js
@@ -0,0 +1,32 @@
+var features={
+  "type": "Feature",
+  "name": "LimesCoordinates_Kleinkastell BBOX",
+  "properties": {},
+  "geometry": {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          7.319315,
+          11.572464
+        ],
+        [
+          50.52089,
+          11.572464
+        ],
+        [
+          50.52089,
+          48.791553
+        ],
+        [
+          11.572464,
+          50.52089
+        ],
+        [
+          7.319315,
+          11.572464
+        ]
+      ]
+    ]
+  }
+}
